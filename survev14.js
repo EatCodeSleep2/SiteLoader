492cbb4433c9e6069dde2274d80460f086681263
I Code <52021568+EatCodeSleep2@users.noreply.github.com>
2025-03-07 15:40:17 -0800

    Update and rename survev13.js to survev14.js

--- survev14.js
+++ survev14.js
@@ -3,6 +3,7 @@ alert("test");
 
 (async function() {
     try {
+        /*
         // Define module URLs
         const vendorModulesUrl = "https://survev.io/vendor-modules-CL-NS4Gd.js";
         const sharedModulesUrl = "https://survev.io/shared-Xqn1Vbd_.js";
@@ -33,7 +34,7 @@ alert("test");
             F: XA, K: XJ, U: XS, L: XB, N: Xc, Q: Xu, V: XV, X: Xz,
             Y: Xo, Z: Xw, _: XO, $: XL, a0: Xv, a1: XK, a2: Xm
         } = shared;
-
+*/
         console.log("✅ All modules loaded successfully!");
 // code goes here
 
